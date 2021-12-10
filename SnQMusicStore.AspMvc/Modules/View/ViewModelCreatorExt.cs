05cbb593af874d4e7d9445d0d31e48c903155b12
Gerhard <g.gehrer@htl-leonding.ac.at>
2021-12-10 15:40:44 +0100

    Updated to the newest framework version.

--- SnQMusicStore.AspMvc/Modules/View/ViewModelCreatorExt.cs
+++ SnQMusicStore.AspMvc/Modules/View/ViewModelCreatorExt.cs
@@ -7,53 +7,53 @@ namespace SnQMusicStore.AspMvc.Modules.View
 {
     partial class ViewModelCreator
     {
-        static partial void BeforeCreateIndexViewModel(ViewBagWrapper viewBagWrapper, IEnumerable<IdentityModel> models, Type modelType, Type displayType, ref IndexViewModel result, ref bool handled)
+        static partial void BeforeCreateIndexViewModel(ViewBagWrapper viewBagInfo, IEnumerable<IdentityModel> models, Type modelType, Type displayType, ref IndexViewModel result, ref bool handled)
         {
             if (modelType.Equals(typeof(Models.Business.App.AlbumTracks)))
             {
                 handled = true;
-                result = new AlbumTracksIndexViewModel(viewBagWrapper, models, modelType, displayType);
+                result = new AlbumTracksIndexViewModel(viewBagInfo, models, modelType, displayType);
             }
             else if (modelType.Equals(typeof(Models.Persistence.App.Track)))
             {
                 handled = true;
-                result = new TrackIndexViewModel(viewBagWrapper, models, modelType, displayType);
+                result = new TrackIndexViewModel(viewBagInfo, models, modelType, displayType);
             }
             else if (modelType.Equals(typeof(Models.Persistence.App.Album)))
             {
                 handled = true;
-                result = new AlbumIndexViewModel(viewBagWrapper, models, modelType, displayType);
+                result = new AlbumIndexViewModel(viewBagInfo, models, modelType, displayType);
             }
         }
-        static partial void BeforeCreateDisplayViewModel(ViewBagWrapper viewBagWrapper, ModelObject model, Type modelType, Type displayType, ref DisplayViewModel result, ref bool handled)
+        static partial void BeforeCreateDisplayViewModel(ViewBagWrapper viewBagInfo, ModelObject model, Type modelType, Type displayType, ref DisplayViewModel result, ref bool handled)
         {
             if (modelType.Equals(typeof(Models.Business.App.TrackAlbumGenre)))
             {
                 handled = true;
-                result = new TrackAlbumGenreDisplayViewModel(viewBagWrapper, model, modelType, displayType);
+                result = new TrackAlbumGenreDisplayViewModel(viewBagInfo, model, modelType, displayType);
             }
             else if (modelType.Equals(typeof(Models.Business.App.AlbumTracks)))
             {
                 handled = true;
-                result = new AlbumDisplayViewModel(viewBagWrapper, model, modelType, displayType);
+                result = new AlbumDisplayViewModel(viewBagInfo, model, modelType, displayType);
             }
             else if (modelType.Equals(typeof(Models.Persistence.App.Track)))
             {
                 handled = true;
-                result = new TrackDisplayViewModel(viewBagWrapper, model, modelType, displayType);
+                result = new TrackDisplayViewModel(viewBagInfo, model, modelType, displayType);
             }
             else if (modelType.Equals(typeof(Models.Persistence.App.Album)))
             {
                 handled = true;
-                result = new AlbumDisplayViewModel(viewBagWrapper, model, modelType, displayType);
+                result = new AlbumDisplayViewModel(viewBagInfo, model, modelType, displayType);
             }
         }
-        static partial void BeforeCreateEditViewModel(ViewBagWrapper viewBagWrapper, IdentityModel model, Type modelType, Type displayType, ref EditViewModel result, ref bool handled)
+        static partial void BeforeCreateEditViewModel(ViewBagWrapper viewBagInfo, IdentityModel model, Type modelType, Type displayType, ref EditViewModel result, ref bool handled)
         {
             if (modelType.Equals(typeof(Models.Persistence.App.Album)))
             {
                 handled = true;
-                result = new AlbumEditViewModel(viewBagWrapper, model, modelType, displayType);
+                result = new AlbumEditViewModel(viewBagInfo, model, modelType, displayType);
             }
         }
     }
